643bc5afd891defc1049f2676e1870275925b448
Aaron Flanagan <aaronflanagan044@hotmail.com>
2015-12-31 21:13:48 +0000

    Ant build file added, jar file for command prompt generated

--- src/ie/gmit/sw/Runner.java
+++ src/ie/gmit/sw/Runner.java
@@ -1,4 +1,4 @@
-package ie.gmit.sw.runner;
+package ie.gmit.sw;
 
 import ie.gmit.sw.image.PaintWordCloud;
 import ie.gmit.sw.utils.*;
@@ -6,7 +6,8 @@ import ie.gmit.sw.utils.*;
 public class Runner {
 	
 	public static void main(String[] args) {
-		IgnoredWords ignoredWords = new IgnoredWords("res/stopwords.txt");
+		IgnoredWords ignoredWords = new IgnoredWords("res/stopwords.txt");//use when running in eclipse
+		//IgnoredWords ignoredWords = new IgnoredWords("./stopwords.txt");//use when running from command prompt
 		WordMap wordMap = new WordMap();
 		
 		Parser parser = new ParserImpl(ignoredWords, wordMap);
