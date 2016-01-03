3c3e7acdcd0bb0198777f997d5bc931e891d25c7
Aaron Flanagan <aaronflanagan044@hotmail.com>
2016-01-03 18:33:31 +0000

    Ant files added in dist, word cloud finished

--- src/ie/gmit/sw/Runner.java
+++ src/ie/gmit/sw/Runner.java
@@ -6,12 +6,12 @@ import ie.gmit.sw.utils.*;
 public class Runner {
 	
 	public static void main(String[] args) {
-		IgnoredWords ignoredWords = new IgnoredWords("res/stopwords.txt");//use when running in eclipse
-		//IgnoredWords ignoredWords = new IgnoredWords("./stopwords.txt");//use when running from command prompt
+		//IgnoredWords ignoredWords = new IgnoredWords("res/stopwords.txt");//use when running in eclipse
+		IgnoredWords ignoredWords = new IgnoredWords("./stopwords.txt");//use when running from command prompt
 		WordMap wordMap = new WordMap();
 		
 		Parser parser = new ParserImpl(ignoredWords, wordMap);
-		PaintWordCloud paintWordCloud = new PaintWordCloud(ignoredWords, wordMap);
+		PaintWordCloud paintWordCloud = new PaintWordCloud(wordMap);
 		
 	}
 
--- src/ie/gmit/sw/image/PaintWordCloud.java
+++ src/ie/gmit/sw/image/PaintWordCloud.java
@@ -2,73 +2,157 @@ package ie.gmit.sw.image;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.imageio.ImageIO;
 
-import ie.gmit.sw.utils.IgnoredWords;
 import ie.gmit.sw.utils.WordMap;
 
+
+/*
+ * PaintWordCloud is a 3 step process.
+ * 1. Strip out the most frequent words from the WordMap
+ * 2. Take these words and sort tem be frequency 
+ * 3. Paint the words into a cloud
+ */
 public class PaintWordCloud {
 	
-	private IgnoredWords ignoredWords;
 	private WordMap wordMap;
 	private BufferedImage image = new BufferedImage(600, 300, BufferedImage.TYPE_4BYTE_ABGR);
 
-	
-	public PaintWordCloud(IgnoredWords ignoredWords, WordMap wordMap) {
-		this.ignoredWords = ignoredWords;
+	public PaintWordCloud(WordMap wordMap) {
 		this.wordMap = wordMap;
 		
-		paint(ignoredWords, wordMap);
+		getMostFrequent(wordMap);
 	}
 	
-	private void paint(IgnoredWords ignoredWords, WordMap wordMap) {
-		Graphics graphics = image.getGraphics();
-		Font font;
+	/*
+	 * Step 1 Strip most frequent words and store
+	 */
+	private void getMostFrequent(WordMap wordMap) {
 		
-		Map.Entry<String, Integer> maxEntry = null;
-		
+		Map<String, Integer> entries = new HashMap<String, Integer>();
+								
 		if(!wordMap.isEmpty()){
+			Map.Entry<String, Integer> maxEntry = null;
 			for (Map.Entry<String, Integer> entry : wordMap.entrySet())
 			{
 			    if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
 			    {
 			    	maxEntry = entry;
-			    	
-			    	if(maxEntry.getValue() >= 50000 && maxEntry.getValue() >= 40000){
-						System.out.println("once " + maxEntry);
-						font = new Font(Font.SANS_SERIF, Font.BOLD, 62);
-						graphics.setColor(Color.blue);
-						graphics.setFont(font);
-						graphics.drawString(maxEntry.getKey(), 0, 100);
-					}
-			    	
-					if(maxEntry.getValue() <= 39999 && maxEntry.getValue() >= 24000){
-						System.out.println("twice " + maxEntry);
-						font = new Font(Font.MONOSPACED, Font.ITALIC, 54);
-						graphics.setColor(Color.green);
-						graphics.setFont(font);
-						graphics.drawString(maxEntry.getKey(), 0, 150);
-					}
-					
-					if(maxEntry.getValue() <= 5 && maxEntry.getValue() >= 1){
-						font = new Font(Font.MONOSPACED, Font.ITALIC, 32);
-						graphics.setColor(Color.yellow);
-						graphics.setFont(font);
-						graphics.drawString(maxEntry.getKey(), 0, 200);
-					}
-					
-					wordMap.removeWord(maxEntry.getKey());
+			    	entries.put(maxEntry.getKey(), maxEntry.getValue());	    				    	
 			    }
 			}
-		}	
+		}
+		
+		sortEntries(entries);
+	}
+	
+	/*
+	 * Step 2: Sort these words by frequency
+	 */
+	private void sortEntries(Map<String, Integer> entries){
+		
+		Comparator<Map.Entry<String, Integer>> byMapValues = new Comparator<Map.Entry<String, Integer>>() {
+	        public int compare(Map.Entry<String, Integer> left, Map.Entry<String, Integer> right) {
+	            return left.getValue().compareTo(right.getValue());
+	        }
+	    };
+		
+		List<Map.Entry<String, Integer>> sortedValues = new ArrayList<Map.Entry<String, Integer>>();
+	    
+	    sortedValues.addAll((Collection<? extends Entry<String, Integer>>) entries.entrySet());
+	 
+	    Collections.sort(sortedValues, byMapValues);//sorts lowest to highest
+	    Collections.reverse(sortedValues);//take sorted values and reverse
+	    	    
+	    paint(sortedValues);
+	}
+	
+	/*
+	 * Step 3: Take the words and paint them to an word cloud image to be outputted
+	 */
+	private void paint(List<Entry<String, Integer>> sortedValues){
+		Graphics2D graphics = (Graphics2D) image.getGraphics();
+		Font font;
+		
+		graphics.setColor(Color.WHITE);
+		graphics.fillRect(0, 0, image.getWidth(), image.getHeight());
+		
+		int loop = 0;
+		
+		while(loop <= 5){
+			if(loop >= sortedValues.size())
+				break;
+			Map.Entry<String, Integer> entry = sortedValues.get(loop);
+			
+			if(loop == 0){
+				font = new Font(Font.DIALOG_INPUT, Font.ITALIC, 100);
+				graphics.setColor(Color.blue);
+				graphics.setFont(font);
+				graphics.drawString(entry.getKey(), 200, 100);
+			}
+			
+			if(loop == 1){
+				font = new Font(Font.SANS_SERIF, Font.ITALIC, 90);
+				graphics.setColor(Color.green);
+				graphics.setFont(font);
+				graphics.drawString(entry.getKey(), 100, 220);
+			}
+			
+			if(loop == 2){
+				font = new Font(Font.MONOSPACED, Font.BOLD, 80);
+				graphics.setColor(Color.red);
+				graphics.setFont(font);
+				graphics.drawString(entry.getKey(), 300, 220);
+			}
+			
+			if(loop == 3){
+				font = new Font(Font.SERIF, Font.ITALIC, 70);
+				graphics.setColor(Color.yellow);
+				graphics.setFont(font);
+				graphics.drawString(entry.getKey(), 220, 155);
+			}
+			
+			if(loop == 4){
+				Graphics2D g2 = (Graphics2D) image.getGraphics();
+				font = new Font(Font.SANS_SERIF, Font.BOLD, 60);
+				g2.setColor(Color.cyan);
+				g2.setFont(font);
+				AffineTransform at = g2.getTransform();
+				at.rotate(Math.PI / 2);
+				g2.setTransform(at);
+				g2.drawString(entry.getKey(), 30, -10);
+				g2.dispose();
+			}
+			
+			if(loop == 5){
+				Graphics2D g2 = (Graphics2D) image.getGraphics();
+				font = new Font(Font.MONOSPACED, Font.ITALIC, 40);
+				g2.setColor(Color.black);
+				g2.setFont(font);
+				AffineTransform at = g2.getTransform();
+				at.rotate(Math.PI / 4);
+				g2.setTransform(at);
+				g2.drawString(entry.getKey(), 295, -270);
+				g2.dispose();
+			}
+			
+			loop++;
+		}
+		
 		try {
 			graphics.dispose();
 			ImageIO.write(image, "png", new File("image.png"));
@@ -76,5 +160,6 @@ public class PaintWordCloud {
 			e.printStackTrace();
 		}
 		System.out.println("Your word cloud has been generated..");
+
 	}
 }
